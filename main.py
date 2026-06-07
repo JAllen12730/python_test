f3ecf4e3586eac9e46c175463178cd768b21f5cd
jamal <jallen12730@yahoo.com>
2026-06-07 13:13:58 -0400

    First Commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('PASS')
+        print('TEST PASSED')
     else:
-        print('FAIL')
+        print('TEST FAILED')
 
 in_autotests_we_trust(10, '10')
 
